779312ffe85824f2d35de33e31e16064796a62be
diegoberser <vegasernajhordie0@gmail.com>
2024-09-11 22:11:17 -0500

    MI PRIMER COMMIT

---
+++ src/main/java/com/prestamo/Dawii2024Semana01Application.java
@@ -0,0 +1,13 @@
+package com.prestamo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dawii2024Semana01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dawii2024Semana01Application.class, args);
+	}
+
+}
